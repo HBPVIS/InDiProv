71699dffe5eb40fb25e428999904ffb4bce95402
cyremur <theshoreoflimbo@gmail.com>
2017-03-01 20:49:08 +0100

    query for client name in test client implement edges in test client adjustments to edge message protocol

--- indiprov/client.cxx
+++ indiprov/client.cxx
@@ -19,9 +19,12 @@
 using namespace std;
 using namespace odb::core;
 
+string clientname;
 const string endpoint("tcp://127.0.0.1:6555");
 shared_ptr<nett::slot_out<Creation>> slotOut;
+bool running = true;
 
+void loginPrompt();
 void mainMenuPrompt();
 void newVertexPrompt();
 void newEdgePrompt();
@@ -30,27 +33,38 @@ int main(int argc, char* argv[]) {
 	nett::initialize(endpoint);
 	slotOut = nett::make_slot_out<Creation>("creation");
 
-	while (true) {
+	loginPrompt();
+	while (running) {
 		mainMenuPrompt();
 	}
 
 	return 0;
 }
 
+void loginPrompt() {
+	cout << "Welcome to InDiProv Logging. Please enter a unique client name." << endl;
+	getline(cin, clientname);
+	cout << endl;
+}
+
 void mainMenuPrompt() {
-	cout << "Welcome to InDiProv Logging. Please choose an option." << endl;
-	cout << "1: new Vertex" << endl;
-	cout << "2: new Edge" << endl;
+	cout << "Please choose an option or enter 'q' to quit." << endl;
+	cout << "0: new Vertex" << endl;
+	cout << "1: new Edge" << endl;
 	char ans = getchar();
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	cout << endl;
 	switch (ans) {
-	case '1':
+	case '0':
 		newVertexPrompt();
 		break;
-	case '2':
+	case '1':
 		newEdgePrompt();
 		break;
+	case 'q':
+		cout << "Shutting down." << endl;
+		running = false;
+		return;
 	default:
 		cout << "Invalid input." << endl << endl;
 		return;
@@ -58,29 +72,32 @@ void mainMenuPrompt() {
 }
 
 void newVertexPrompt() {
-	cout << "Please choose a Vertex Type." << endl;
-	cout << "1: Activity" << endl;
-	cout << "2: Agent" << endl;
-	cout << "3: Entity" << endl;
+	cout << "Please choose a Vertex Type or enter 'q' to quit." << endl;
+	cout << "0: Activity" << endl;
+	cout << "1: Agent" << endl;
+	cout << "2: Entity" << endl;
 	char ans = getchar();
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	cout << endl;
 	Creation message;
+	message.set_clientname(clientname);
 	Creation::NewVertex* vert = message.add_vertices();
 	string name;
 	switch (ans) {
-	case '1':
+	case '0':
 		vert->set_type(Creation::Activity);
 		cout << "Please enter Activity name" << endl;
 		break;
-	case '2':
+	case '1':
 		vert->set_type(Creation::Agent);
 		cout << "Please enter Agent name" << endl;
 		break;
-	case '3':
+	case '2':
 		vert->set_type(Creation::Entity);
 		cout << "Please enter Entity name" << endl;
 		break;
+	case 'q':
+		return;
 	default:
 		cout << "Invalid input." << endl;
 		return;
@@ -96,5 +113,57 @@ void newVertexPrompt() {
 }
 
 void newEdgePrompt() {
-	cout << "Not yet implemented." << endl << endl;
+	cout << "Please choose an Edge Type or enter 'q' to quit." << endl;
+	cout << "0: wasGeneratedBy" << endl;
+	cout << "1: wasDerivedFrom" << endl;
+	cout << "2: wasAttributedTo" << endl;
+	cout << "3: used" << endl;
+	cout << "4: wasInformedBy" << endl;
+	cout << "5: wasAssociatedWith" << endl;
+	cout << "6: actedOnBehalfOf" << endl;
+	char ans = getchar();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	cout << endl;
+	Creation message;
+	message.set_clientname(clientname);
+	Creation::NewEdge* edge = message.add_edges();
+	switch (ans) {
+	case '0':
+		edge->set_type(Creation::wasGeneratedBy);
+		break;
+	case '1':
+		edge->set_type(Creation::wasDerivedFrom);
+		break;
+	case '2':
+		edge->set_type(Creation::wasAttributedTo);
+		break;
+	case '3':
+		edge->set_type(Creation::used);
+		break;
+	case '4':
+		edge->set_type(Creation::wasInformedBy);
+		break;
+	case '5':
+		edge->set_type(Creation::wasAssociatedWith);
+		break;
+	case '6':
+		edge->set_type(Creation::actedOnBehalfOf);
+		break;
+	case 'q':
+		return;
+	default:
+		cout << "Invalid input." << endl;
+		return;
+	}
+	string name1, name2;
+	cout << "Please enter first Vertex name." << endl;
+	getline(cin, name1);
+	cout << endl;
+	cout << "Please enter second Vertex name." << endl;
+	getline(cin, name2);
+	cout << endl;
+	edge->set_firstvertname(name1);
+	edge->set_secondvertname(name2);
+
+	cout << "Sending Edge log info: " << name1 << " " << edgeTypeToString((edgeType)edge->type()) << " " << name2 << endl << endl;
 }
